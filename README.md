be16187988e85e6aa35f3b88d61fe1632eff5a17
Aslak Johansen <aslakj@gmail.com>
2020-05-13 19:38:50 +0200

    Added http server and demo handler

--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 ## Requirements
 
 - Python 3.5+
+- Python modules:
+  - `asyncio`
+  - `aiohttp`
+  - `json`
+
+## Running
+
+```shell
+$ ./rdf-server.py
+Syntax: ./rdf-server.py INTERFACE PORT MODEL_DIR
+        ./rdf-server.py 0.0.0.0 8001 ../var/model
+$ ./rdf-server.py 0.0.0.0 8001 ../var/model
+```
+
+## Interface
+
+### Fetch time from server
+
+```shell
+$ curl -X PUT -d '42' http://localhost:8001/time
+{
+    "success": true,
+    "time": 1589391233.535275
+}
+```
+
+**Note:** The `42` payload is not used, but the payload needs to be valid json.
+
--- src/rdf-server.py
+++ src/rdf-server.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
+import asyncio
+from aiohttp import web
+import json
+from time import time
+
+dispatch = {}
 
 ###############################################################################
 ####################################################################### helpers
@@ -11,13 +17,77 @@ def valid_python_version ():
     if v[1] < 5: return False
     return True
 
+def register_handler (path: str, handler):
+    dispatch[path] = handler
+
+###############################################################################
+###################################################################### handlers
+
+async def handler_time (path: str, payload):
+    message = json.dumps({
+        'success': True,
+        'time': time(),
+    }, sort_keys=True, indent=4, separators=(',', ': '))
+    return web.Response(status=200, text=message)
+
+async def handler (request: web.Request):
+    method  =       request.method
+    path    =   str(request.rel_url)[1:]
+    payload = await request.content.read()
+    
+    payload = json.loads(payload.decode('utf-8'))
+    
+    # produce answer
+    if path in dispatch:
+        return await dispatch[path](path, payload)
+    else:
+        message = json.dumps({
+            'success': False,
+            'error': {
+                'description': 'No handler registered for path',
+                'method':      method,
+                'path':        path,
+            },
+        }, sort_keys=True, indent=4, separators=(',', ': '))
+        return web.Response(status=404, text=message)
 
 ###############################################################################
 ########################################################################## main
+
+async def main(interface: str, port: int):
+    proto  = web.Server(handler)
+    server = await loop.create_server(proto, interface, port)
 
 # guard: python version
 if not valid_python_version():
     print('ERROR: Invalid python version (%s), bust be 3.(5+).' % str(sys.version_info))
     sys.exit(1)
 
+# guard: commandline arguments
+if len(sys.argv) != 4:
+    print('Syntax: %s INTERFACE PORT MODEL_DIR' % sys.argv[0])
+    print('        %s 0.0.0.0 8001 ../var/model' % sys.argv[0])
+    sys.exit(2)
+
+# extract parameters
+interface =     sys.argv[1]
+port      = int(sys.argv[2])
+model_dir =     sys.argv[3]
+
+# register handlers
+register_handler('time', handler_time)
+
+loop = asyncio.get_event_loop()
+asyncio.Task(main(interface, port))
+
+# enter service loop
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    print("STATUS: Exiting ...")
+    loop.close()
+    exit(0)
+
+########################################################################### EOF
+###############################################################################
 
