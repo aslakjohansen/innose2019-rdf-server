4c431b6c96b8dc4a95f9cf6acb42c9079d32d80a
Aslak Johansen <aslakj@gmail.com>
2020-07-31 14:05:00 +0200

    Added empty handlers for all targets

--- src/rdf-server.go
+++ src/rdf-server.go
@@ -10,11 +10,35 @@ func time_handler (rw http.ResponseWriter, request *http.Request) {
     rw.Write([]byte(fmt.Sprintf("%f",result)))
 }
 
+func store_handler (rw http.ResponseWriter, request *http.Request) {
+    var result float64 = 42.56
+    rw.Write([]byte(fmt.Sprintf("%f",result)))
+}
+
+func namespace_handler (rw http.ResponseWriter, request *http.Request) {
+    var result float64 = 42.56
+    rw.Write([]byte(fmt.Sprintf("%f",result)))
+}
+
+func query_handler (rw http.ResponseWriter, request *http.Request) {
+    var result float64 = 42.56
+    rw.Write([]byte(fmt.Sprintf("%f",result)))
+}
+
+func update_handler (rw http.ResponseWriter, request *http.Request) {
+    var result float64 = 42.56
+    rw.Write([]byte(fmt.Sprintf("%f",result)))
+}
+
 func main () {
     var port int16 = 8001
     
     go func () {
-        http.HandleFunc("/time", time_handler)
+        http.HandleFunc("/time"      , time_handler)
+        http.HandleFunc("/store"     , store_handler)
+        http.HandleFunc("/namespaces", namespace_handler)
+        http.HandleFunc("/query"     , query_handler)
+        http.HandleFunc("/update"    , update_handler)
         
         fmt.Println(fmt.Sprintf("Listening to localhost:%d", port))
         
