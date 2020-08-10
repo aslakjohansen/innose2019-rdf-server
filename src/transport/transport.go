293f8bf0f0c963d4f50520f34246edf2b986289d
Aslak Johansen <aslakj@gmail.com>
2020-08-11 01:35:54 +0200

    Cleanup

--- src/transport/transport.go
+++ src/transport/transport.go
@@ -55,13 +55,6 @@ func time_handler (rw http.ResponseWriter, request *http.Request) {
 func store_handler (rw http.ResponseWriter, request *http.Request) {
     var result string = command.Store("", model_dir)
     rw.Write([]byte(result))
-//    var success bool
-//    var result string
-//    result, success = logic.Store(*model_dir)
-//    rw.Write([]byte("{\n"))
-//    rw.Write([]byte(fmt.Sprintf("    \"success\": %t,\n", success)))
-//    rw.Write([]byte(fmt.Sprintf("    \"filename\": %s\n", result)))
-//    rw.Write([]byte("}\n"))
 }
 
 func namespace_handler (rw http.ResponseWriter, request *http.Request) {
