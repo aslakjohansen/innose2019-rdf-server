dbcdd2ec04fcfccc51f2cf33a958443a2d084dab
Aslak Johansen <aslakj@gmail.com>
2020-08-12 23:11:25 +0200

    Added a few test cases

--- test/parse-sparql.go
+++ test/parse-sparql.go
@@ -10,18 +10,20 @@ func main () {
     var lexer = sparql.NewLexer(true)
     
     var inputs []string = []string{
+      "garbage",
       "SELECT ?var1 ?var2 ?var3 WHERE { ?var1 ?var2 ?var3 . }",
       "SELECT ?var1 ?var2 ?var3 WHERE { ?var1 (?var2) ?var3 . }",
       "SELECT ?var1 ?var2 ?var3 WHERE { ?var1 ?var2/?var2 ?var3 . }",
       "SELECT ?var1 ?var2 ?var3 WHERE { ?var1 ?var2|?var3 ?var4 . }",
       "SELECT ?var1 ?var2 ?var3 WHERE { ?var1 (?var2|?var3) ?var4 . }",
       "SELECT ?var1 ?var2 ?var3 WHERE { ?var1 ?var2+ ?var3 . }",
+      "SELECT ?var1 ?var2 ?var3 WHERE { ?var1 ?var2++ ?var3 . }",
       "SELECT ?var1 ?var2 ?var3 WHERE { ?var1 ?var2* ?var3 . }",
       "SELECT ?var1 ?var2 ?var3 WHERE { ?var1 (?var2|?var3)/?var4 ?var5 . }",
     }
     
     for _, input := range inputs {
-        fmt.Println(input)
+        fmt.Println("Case:", input)
         line, err := sparql.Parse(lexer, input)
         if err != nil {
             fmt.Println(">> Error parsing:", err)
