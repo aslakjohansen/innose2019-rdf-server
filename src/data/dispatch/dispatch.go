8e3a1b71611a0927a3888e48ec19811e1428d9ba
Aslak Johansen <aslakj@gmail.com>
2020-08-28 16:43:36 +0200

    Cleanup

--- src/data/dispatch/dispatch.go
+++ src/data/dispatch/dispatch.go
@@ -25,31 +25,25 @@ func NewDispatcher () *Dispatcher {
 
 func (d Dispatcher) Register (id string, channel chan reading.Reading) chan reading.Reading {
     d.mutex.Lock()
+    
     entries, ok := d.lut[id]
     if !ok {
-        fmt.Println("notok a")
         newalloc := make([]chan reading.Reading, 0)
         entries = &newalloc
         d.lut[id] = entries
-        fmt.Println(d.lut[id])
-        fmt.Println(entries)
-        fmt.Println("notok b")
     }
-    fmt.Println(entries)
+    
     if !contains(entries, channel) {
-        fmt.Println("Appending")
-        fmt.Println(entries)
         *entries = append(*entries, channel)
-        fmt.Println(entries)
     }
-//    d.lut[id] = channel
+    
     d.mutex.Unlock()
     return channel
 }
 
 func (d Dispatcher) Dispatch (id string, r *reading.Reading) bool {
     channels, ok := d.lut[id]
-    d.Print()
+    
     if ok {
         for _, channel := range *channels {
             channel <- *r
@@ -69,7 +63,6 @@ func (d Dispatcher) Print () {
 /////////////////////////////////////////////////////////////////////// helpers
 
 func contains (array *[]chan reading.Reading, value chan reading.Reading) bool {
-    fmt.Println(array)
     for _, current := range *array {
         if current==value {
             return true
