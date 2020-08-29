89abe49d1885d05bb10a33de892ecdc38b5c644e
Aslak Johansen <aslakj@gmail.com>
2020-08-29 11:20:37 +0200

    Moved channels into arrays

--- test/data-dispatch-registration.go
+++ test/data-dispatch-registration.go
@@ -9,19 +9,37 @@ import (
 
 func main () {
     var d *dispatch.Dispatcher = dispatch.NewDispatcher()
+    var as [] chan reading.Reading = [] chan reading.Reading {
+        make(chan reading.Reading),
+        make(chan reading.Reading),
+        make(chan reading.Reading),
+        make(chan reading.Reading),
+    }
+    var bs [] chan reading.Reading = [] chan reading.Reading {
+        make(chan reading.Reading),
+        make(chan reading.Reading),
+        make(chan reading.Reading),
+    }
+    var cs [] chan reading.Reading = [] chan reading.Reading {
+        make(chan reading.Reading),
+        make(chan reading.Reading),
+    }
+    var ds [] chan reading.Reading = [] chan reading.Reading {
+        make(chan reading.Reading),
+    }
     
     fmt.Println("Adding:")
     fmt.Println("~~~~~~")
-    d.Register("a", make(chan reading.Reading))
-    d.Register("a", make(chan reading.Reading))
-    d.Register("a", make(chan reading.Reading))
-    d.Register("b", make(chan reading.Reading))
-    d.Register("b", make(chan reading.Reading))
-    d.Register("c", make(chan reading.Reading))
-    d.Register("a", make(chan reading.Reading))
-    d.Register("b", make(chan reading.Reading))
-    d.Register("c", make(chan reading.Reading))
-    d.Register("d", make(chan reading.Reading))
+    d.Register("a", as[0])
+    d.Register("a", as[1])
+    d.Register("a", as[2])
+    d.Register("b", bs[0])
+    d.Register("b", bs[1])
+    d.Register("c", cs[0])
+    d.Register("a", as[3])
+    d.Register("b", bs[2])
+    d.Register("c", cs[1])
+    d.Register("d", ds[0])
     d.Print()
     fmt.Println("")
 }
