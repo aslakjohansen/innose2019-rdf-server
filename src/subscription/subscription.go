17e8155dfbcf939bcb76d6e6ff6dcc6c4fd86ac1
Aslak Johansen <aslakj@gmail.com>
2020-10-12 22:23:03 +0200

    First shot at a subscription implementation
    Unsubscribe is still empty

--- src/subscription/subscription.go
+++ src/subscription/subscription.go
@@ -2,24 +2,226 @@ package subscription
 
 import (
     "fmt"
+    "sync"
+    
+    // "innose2019-rdf-server/sparql"
+    "innose2019-rdf-server/logic"
 )
 
+var (
+    dispatch_mux sync.Mutex
+    dispatch map[string](*DispatchEntry)
+)
+
+// type ResultSet ([][]string)
+
+type DispatchEntry struct {
+    Cache            *([][]string)
+    Subscriptions *[]*Subscription
+}
+
+type ResultDiff struct {
+    Plus  *([][]string) `json:"+"`
+    Minus *([][]string) `json:"-"`
+}
+
 type Subscription struct {
+    id              string
+    Query           string
     ResponseChannel chan []byte
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////// resultset
+
+func NewResultDiff () *ResultDiff {
+    var result ResultDiff
+    result.Plus  = nil
+    result.Minus = nil
+    return &result
+}
+
+func (r *ResultDiff) Transmit (channel chan []byte, id string) {
+    var response string = ""
+    response += fmt.Sprintf("{\n")
+    response += fmt.Sprintf("  \"type\": \"resultset\"\n")
+    response += fmt.Sprintf("  \"id\": \"%s\"\n", id)
+    response += fmt.Sprintf("  \"+\": [\n")
+    for i, row := range *r.Plus {
+        response += fmt.Sprintf("    [\n")
+        for j, cell := range row {
+            response += fmt.Sprintf("      \"%s\"", cell)
+            if j!=len(row)-1 {
+                response += fmt.Sprintf(",")
+            }
+            response += fmt.Sprintf("\n")
+        }
+        response += fmt.Sprintf("    ]")
+        if i!=len(*r.Plus)-1 {
+            response += fmt.Sprintf(",")
+        }
+        response += fmt.Sprintf("\n")
+    }
+    response += fmt.Sprintf("  ],\n")
+    response += fmt.Sprintf("  \"-\": [\n")
+    for i, row := range *r.Minus {
+        response += fmt.Sprintf("    [\n")
+        for j, cell := range row {
+            response += fmt.Sprintf("      \"%s\"", cell)
+            if j!=len(row)-1 {
+                response += fmt.Sprintf(",")
+            }
+            response += fmt.Sprintf("\n")
+        }
+        response += fmt.Sprintf("    ]")
+        if i!=len(*r.Minus)-1 {
+            response += fmt.Sprintf(",")
+        }
+        response += fmt.Sprintf("\n")
+    }
+    response += fmt.Sprintf("  ]\n")
+    response += fmt.Sprintf("}")
+    
+    channel <- []byte(response)
+}
+
+func resultset_diff (a *([][]string), b *([][]string)) *ResultDiff {
+    var result ResultDiff
+    var found_row bool
+    
+    // TODO: will this work?
+    result.Plus  = nil
+    result.Minus = nil
+    
+    // fill out.Transmit minus
+    for _, rowa := range *a {
+        found_row = false
+        for _, rowb := range *b {
+            var match bool = true
+            for i, elema := range rowa {
+                elemb := rowb[i]
+                if elema != elemb {
+                    match = false
+                    break
+                }
+            }
+            if match {
+                found_row = true
+                break
+            }
+        }
+        if !found_row {
+            tmp := append(*result.Minus, rowa)
+            result.Minus = &tmp
+        }
+    }
+    
+    // fill out plus
+    for _, rowb := range *a {
+        found_row = false
+        for _, rowa := range *b {
+            var match bool = true
+            for i, elemb := range rowb {
+                elema := rowa[i]
+                if elemb != elema {
+                    match = false
+                    break
+                }
+            }
+            if match {
+                found_row = true
+                break
+            }
+        }
+        if !found_row {
+            tmp := append(*result.Plus, rowb)
+            result.Plus = &tmp
+        }
+    }
+    
+    return &result
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////// dispatch entry
+
+func NewDispatchEntry () *DispatchEntry {
+    var de DispatchEntry
+    
+    de.Cache = nil
+    tmp := make([]*Subscription, 0)
+    de.Subscriptions = &tmp
+    
+    return &de
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////// interface functions
 
-func NewSubscription (response_channel chan []byte) *Subscription {
+func Update () {
+    for query, de := range dispatch {
+        var result  [][]string
+        var success bool
+        result, success = logic.Query(query)
+        
+        // guard: no success
+        if !success {
+            fmt.Println("Unable to perform query during update.")
+            continue
+        }
+        
+        var diff *ResultDiff = resultset_diff(de.Cache, &result)
+        
+        for _, subscription := range *de.Subscriptions {
+            diff.Transmit(subscription.ResponseChannel, subscription.id)
+        }
+    }
+}
+
+func Unsubscribe (id string) bool {
+    // TODO: This id is only unique through the context of a session. The session should have an id->subscription dictionary and call destroy on unsubscription
+    return true
+}
+
+func NewSubscription (id string, query string, response_channel chan []byte) *Subscription {
     var s Subscription
     
+    s.id              = id
+    s.Query           = query
     s.ResponseChannel = response_channel
+    
+    // add to dispatch data structure
+    dispatch_mux.Lock()
+    defer dispatch_mux.Unlock()
+    de, ok := dispatch[query]
+    if !ok {
+        result, success := logic.Query(query)
+        if !success {
+            fmt.Println("TODO: send back an error message from subscription.go:NewSubscription")
+            return nil // TODO: Check this on the caller side
+        }
+        de = NewDispatchEntry()
+        de.Cache = &result
+        dispatch[query] = de
+    }
+    tmp := append(*de.Subscriptions, &s)
+    de.Subscriptions = &tmp
     
     return &s
 }
 
 func (s *Subscription) Destroy () {
+    // remove from dispatch data structure
+    dispatch_mux.Lock()
+    subscriptions := dispatch[s.Query].Subscriptions
+    for i, elem := range *subscriptions {
+        if elem==s {
+            tmp := append((*subscriptions)[:i], (*subscriptions)[i+1:]...)
+            dispatch[s.Query].Subscriptions = &tmp
+            break
+        }
+    }
+    dispatch_mux.Unlock()
 }
 
 func (s *Subscription) String () string {
@@ -28,5 +230,20 @@ func (s *Subscription) String () string {
     result += fmt.Sprintf("subscription")
     
     return result
+}
+
+func (s *Subscription) Push () {
+    dispatch_mux.Lock()
+    defer dispatch_mux.Unlock()
+    
+    de, success := dispatch[s.Query]
+    if !success {
+        fmt.Println("Internal error detected in Subscription.Push.")
+        return
+    }
+    
+    var result *ResultDiff = NewResultDiff()
+    result.Plus = de.Cache
+    result.Transmit(s.ResponseChannel, s.id)
 }
 
--- src/transport/dispatch.go
+++ src/transport/dispatch.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "encoding/json"
     
+    "innose2019-rdf-server/sparql"
     "innose2019-rdf-server/logic"
     "innose2019-rdf-server/session"
     "innose2019-rdf-server/subscription"
@@ -18,45 +19,72 @@ type Entry struct {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////// helpers
+
+func send_response (channel chan []byte, id string, response string) {
+    var message string = ""
+    message += fmt.Sprintf("{\n")
+    message += fmt.Sprintf("    \"id\": \"%s\",\n", id)
+    message += fmt.Sprintf("    \"response\": %s\n", response)
+    message += fmt.Sprintf("}\n")
+    
+    channel <- []byte(message)
+}
+
+func send_response_error (channel chan []byte, id string, details string) {
+    var message string = ""
+    message += fmt.Sprintf("{\n")
+    message += fmt.Sprintf("    \"id\": \"%s\",\n", id)
+    message += fmt.Sprintf("    \"response\": \"%s\",\n", "error")
+    message += fmt.Sprintf("    \"details\": \"%s\"\n", details)
+    message += fmt.Sprintf("}\n")
+    
+    channel <- []byte(message)
+}
+
+///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////// command types
 
 type TimeEntry struct {
     Entry
 }
 func (e *TimeEntry) Handle (s *session.Session) {
-    var response string = ""
-    response += fmt.Sprintf("{\n")
-    response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
-    response += fmt.Sprintf("    \"response\": %s\n", logic.JsonTime("    "))
-    response += fmt.Sprintf("}\n")
+    send_response(s.ResponseChannel, e.Identifier, logic.JsonTime("    "))
+    // var response string = ""
+    // response += fmt.Sprintf("{\n")
+    // response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
+    // response += fmt.Sprintf("    \"response\": %s\n", logic.JsonTime("    "))
+    // response += fmt.Sprintf("}\n")
     
-    s.ResponseChannel <- []byte(response)
+    // s.ResponseChannel <- []byte(response)
 }
 
 type StoreEntry struct {
     Entry
 }
 func (e *StoreEntry) Handle (s *session.Session) {
-    var response string = ""
-    response += fmt.Sprintf("{\n")
-    response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
-    response += fmt.Sprintf("    \"response\": %s\n", logic.JsonStore("    ", model_dir))
-    response += fmt.Sprintf("}\n")
+    send_response(s.ResponseChannel, e.Identifier, logic.JsonStore("    ", model_dir))
+    // var response string = ""
+    // response += fmt.Sprintf("{\n")
+    // response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
+    // response += fmt.Sprintf("    \"response\": %s\n", logic.JsonStore("    ", model_dir))
+    // response += fmt.Sprintf("}\n")
     
-    s.ResponseChannel <- []byte(response)
+    // s.ResponseChannel <- []byte(response)
 }
 
 type NamespacesEntry struct {
     Entry
 }
 func (e *NamespacesEntry) Handle (s *session.Session) {
-    var response string = ""
-    response += fmt.Sprintf("{\n")
-    response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
-    response += fmt.Sprintf("    \"response\": %s\n", logic.JsonNamespaces("    "))
-    response += fmt.Sprintf("}\n")
+    send_response(s.ResponseChannel, e.Identifier, logic.JsonNamespaces("    "))
+    // var response string = ""
+    // response += fmt.Sprintf("{\n")
+    // response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
+    // response += fmt.Sprintf("    \"response\": %s\n", logic.JsonNamespaces("    "))
+    // response += fmt.Sprintf("}\n")
     
-    s.ResponseChannel <- []byte(response)
+    // s.ResponseChannel <- []byte(response)
 }
 
 type QueryEntry struct {
@@ -64,13 +92,14 @@ type QueryEntry struct {
     Query string `json:"query"`
 }
 func (e *QueryEntry) Handle (s *session.Session) {
-    var response string = ""
-    response += fmt.Sprintf("{\n")
-    response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
-    response += fmt.Sprintf("    \"response\": %s\n", logic.JsonQuery("    ", e.Query))
-    response += fmt.Sprintf("}\n")
+    send_response(s.ResponseChannel, e.Identifier, logic.JsonQuery("    ", e.Query))
+    // var response string = ""
+    // response += fmt.Sprintf("{\n")
+    // response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
+    // response += fmt.Sprintf("    \"response\": %s\n", logic.JsonQuery("    ", e.Query))
+    // response += fmt.Sprintf("}\n")
     
-    s.ResponseChannel <- []byte(response)
+    // s.ResponseChannel <- []byte(response)
 }
 
 type UpdateEntry struct {
@@ -78,13 +107,18 @@ type UpdateEntry struct {
     Query string `json:"query"`
 }
 func (e *UpdateEntry) Handle (s *session.Session) {
-    var response string = ""
-    response += fmt.Sprintf("{\n")
-    response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
-    response += fmt.Sprintf("    \"response\": %s\n", logic.JsonUpdate("    ", e.Query))
-    response += fmt.Sprintf("}\n")
+    var response string = logic.JsonUpdate("    ", e.Query)
+    subscription.Update()
+    send_response(s.ResponseChannel, e.Identifier, response)
+    // var response string = ""
+    // response += fmt.Sprintf("{\n")
+    // response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
+    // response += fmt.Sprintf("    \"response\": %s\n", logic.JsonUpdate("    ", e.Query))
+    // response += fmt.Sprintf("}\n")
     
-    s.ResponseChannel <- []byte(response)
+    // subscription.Update()
+    
+    // s.ResponseChannel <- []byte(response)
 }
 
 type InspectEntry struct {
@@ -92,13 +126,14 @@ type InspectEntry struct {
     Query string `json:"query"`
 }
 func (e *InspectEntry) Handle (s *session.Session) {
-    var response string = ""
-    response += fmt.Sprintf("{\n")
-    response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
-    response += fmt.Sprintf("    \"response\": %s\n", logic.JsonInspect("    ", e.Query))
-    response += fmt.Sprintf("}\n")
+    send_response(s.ResponseChannel, e.Identifier, logic.JsonInspect("    ", e.Query))
+    // var response string = ""
+    // response += fmt.Sprintf("{\n")
+    // response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
+    // response += fmt.Sprintf("    \"response\": %s\n", logic.JsonInspect("    ", e.Query))
+    // response += fmt.Sprintf("}\n")
     
-    s.ResponseChannel <- []byte(response)
+    // s.ResponseChannel <- []byte(response)
 }
 
 type SubscribeEntry struct {
@@ -106,16 +141,39 @@ type SubscribeEntry struct {
     Query string `json:"query"`
 }
 func (e *SubscribeEntry) Handle (s *session.Session) {
-    var sub *subscription.Subscription = subscription.NewSubscription(s.ResponseChannel)
+    var lexer = sparql.NewLexer(true)
+    var node *sparql.Node
+    var err   error
+    var q     string
+    
+    // parse tree
+    node, err = sparql.Parse(lexer, e.Query)
+    if err != nil {
+        send_response_error(s.ResponseChannel, e.Identifier, "[RESPARQL] Parse error:"+err.Error())
+        return
+    }
+    
+    // extract pure sparql
+    q, err = node.Resparql("")
+    if err != nil {
+        send_response_error(s.ResponseChannel, e.Identifier, "[RESPARQL] Sparqlification error:"+err.Error())
+        return
+    }
+    
+    var sub *subscription.Subscription = subscription.NewSubscription(e.Identifier, q, s.ResponseChannel)
     s.AddSubscription(e.Identifier, sub)
     
-    var response string = ""
-    response += fmt.Sprintf("{\n")
-    response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
-    response += fmt.Sprintf("    \"response\": \"%s\"\n", "subscribed")
-    response += fmt.Sprintf("}\n")
+    send_response(s.ResponseChannel, e.Identifier, "subscribed")
     
-    s.ResponseChannel <- []byte(response)
+    sub.Push()
+    
+    // var response string = ""
+    // response += fmt.Sprintf("{\n")
+    // response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
+    // response += fmt.Sprintf("    \"response\": \"%s\"\n", "subscribed")
+    // response += fmt.Sprintf("}\n")
+    
+    // s.ResponseChannel <- []byte(response)
 }
 
 type UnsubscribeEntry struct {
@@ -123,15 +181,17 @@ type UnsubscribeEntry struct {
     Subscription string `json:"subscription"`
 }
 func (e *UnsubscribeEntry) Handle (s *session.Session) {
-    s.RemoveSubscription(e.Subscription)
+    subscription.Unsubscribe(e.Identifier)
+    send_response(s.ResponseChannel, e.Identifier, "unsubscribed")
+    // s.RemoveSubscription(e.Subscription)
     
-    var response string = ""
-    response += fmt.Sprintf("{\n")
-    response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
-    response += fmt.Sprintf("    \"response\": \"%s\"\n", "unsubscribed")
-    response += fmt.Sprintf("}\n")
+    // var response string = ""
+    // response += fmt.Sprintf("{\n")
+    // response += fmt.Sprintf("    \"id\": \"%s\",\n", e.Identifier)
+    // response += fmt.Sprintf("    \"response\": \"%s\"\n", "unsubscribed")
+    // response += fmt.Sprintf("}\n")
     
-    s.ResponseChannel <- []byte(response)
+    // s.ResponseChannel <- []byte(response)
 }
 
 type SubscriptionsEntry struct {
